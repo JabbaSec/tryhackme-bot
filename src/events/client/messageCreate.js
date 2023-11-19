c4b98c323ece4024cf0a54f7d7a729429962e901
JabbaSec <68778279+JabbaSec@users.noreply.github.com>
2023-11-19 00:16:52 +0000

    Removed token check from messageCreate.js

--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -1,21 +1,5 @@
-const TOKEN_PATTERN = /[a-zA-Z0-9]{24}/;
-
 module.exports = {
   name: "messageCreate",
 
-  async execute(message, client) {
-    // Attempt to detect a token being sent and remove it
-    if (TOKEN_PATTERN.test(message.content)) {
-      try {
-        await message.delete();
-        const userId = message.author.id;
-
-        message.channel.send(
-          `<@${userId}>, Oops! It looks like you sent something similar to a TryHackMe Discord Token, I have removed your message to be safe.\nIf this was a mistake, please let <@${process.env.BOT_DEVELOPER_ID}> know what your message was.`
-        );
-      } catch (error) {
-        console.error("Error deleting message:", error);
-      }
-    }
-  },
+  async execute(message, client) {},
 };
