3e5164325a64c16963ccf2c6588211f259bd7010
JabbaSec <68778279+JabbaSec@users.noreply.github.com>
2023-11-24 23:24:41 +0000

    Updated bot's status

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -5,12 +5,8 @@ module.exports = {
   once: true,
 
   async execute(client) {
-    guild = client.guilds.cache.get(process.env.GUILD_ID);
-
     console.log(`${client.user.username} is ready`);
 
-    client.user.setActivity(`${guild.memberCount} users`, {
-      type: ActivityType.Watching,
-    });
+    client.user.setActivity(`https://tryhackme.com/`);
   },
 };
